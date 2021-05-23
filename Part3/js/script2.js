aef82fd96c9b7e768e3c46db96b47ad63db6fe7e
IrynaSolonenko <iryna17z89@gmail.com>
2021-05-23 21:53:53 +0300

    fixed

---
+++ Part3/js/script2.js
@@ -0,0 +1,10 @@
+const surname = prompt('Напишите вашу фамилию');
+const name = prompt('Напишите ваше имя');
+const fathername = prompt('Напишите ваше отчество');
+
+console.log(surname, name, fathername);
+
+const result = surname + name + fathername ;
+
+console.log (result);
+alert('Ваши данные: ' + surname + ' ' + name + ' ' + fathername);
