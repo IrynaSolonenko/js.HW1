2c936c97c88c7a9dc68eb90d3ba7980ecac26580
IrynaSolonenko <iryna17z89@gmail.com>
2021-05-23 14:54:14 +0300

    confirm

---
+++ Part2/js/script.js
@@ -0,0 +1,8 @@
+const number1 = prompt('Напишите первое число');
+const number2 = prompt('Напишите второе число');
+
+console.log(number1, number2);
+
+const result = number1 - number2;
+console.log(result);
+alert('Результат вычитания ' + result);
